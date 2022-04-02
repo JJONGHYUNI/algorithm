9d3d884aae25c4fd501dc67cf7310ece619360b3
JJONGHYUNI <whdgusdl8792@gmail.com>
2022-04-03 00:46:33 +0900

    [.]

---
+++ stack/ironStick(Stack).py
@@ -0,0 +1,14 @@
+import sys
+razor=input()
+answer=0
+index=[]
+for i in range(len(razor)):
+    if razor[i]=="(":
+        index.append(razor[i])
+        if razor[i+1]==")":
+            index.pop()
+            answer+=len(index)
+    elif razor[i]==")"and razor[i-1]==")":
+        index.pop()
+        answer+=1
+print(answer)
---
+++ stack/ironStick.py
@@ -0,0 +1,14 @@
+import sys
+razor=input()
+answer=0
+index=0
+for i in range(len(razor)):
+    if razor[i]=="(":
+        index+=1
+        if razor[i+1]==")":
+            index-=1
+            answer+=index
+    elif razor[i]==")"and razor[i-1]==")":
+        index-=1
+        answer+=1
+print(answer)
