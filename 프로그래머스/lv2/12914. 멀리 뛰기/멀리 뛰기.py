baaa70191d3e56b47ccc11494d3fba29d8cf43aa
JJONGHYUNI <whdgusdl8792@gmail.com>
2023-04-18 14:54:55 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.47 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -0,0 +1,11 @@
+
+def solution(n):
+    answer = 0
+    if n==1:
+        return n
+    dp=[0]*(n+1)
+    dp[1],dp[2]=1,2
+    for i in range(3,n+1):
+        dp[i]=(dp[i-1]+dp[i-2])%1234567
+    answer=dp[n]
+    return answer
