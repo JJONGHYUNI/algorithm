947372e581293e0f433f0fb6ee4e087097a15ab8
JJONGHYUNI <whdgusdl8792@gmail.com>
2023-10-09 20:09:17 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.02 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] solution(int n, int m) {
+        int[] answer = {gcd(n, m) , lcm(n, m)};
+        return answer;
+    }
+    
+    private int gcd(int a, int b){
+        if(b == 0){
+            return a;
+        }
+        return gcd(b, a%b);
+    }
+    
+    private int lcm(int a, int b){
+        return a * b / gcd(a, b);
+    }
+}
