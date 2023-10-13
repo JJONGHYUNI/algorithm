281378a023eb03b27a4becdd992d629e36960836
JJONGHYUNI <whdgusdl8792@gmail.com>
2023-10-13 15:44:57 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.06 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -2,26 +2,8 @@ import java.util.*;
 
 class Solution {
     public int solution(int n) {
-        long three = convertToThreeAndReverse(n);
-        long answer = convertToDemical(Long.toString(three));
-        return (int) answer;
-    }
-    private long convertToThreeAndReverse(long n){
-        String result = "";
-        while(n / 3 != 0){
-            result += Long.toString(n % 3);
-            n /= 3;
-        }
-        result += Long.toString(n%3);
-        
-        return Long.parseLong(result);
-    }
-    private long convertToDemical(String str){
-        long result = 0 ;
-        for(int i = 0; i < str.length(); i++){
-            int temp = (int) Math.pow(3, i);
-            result += temp * (int) (str.charAt(str.length() - 1 - i) - '0');
-        }
-        return result;
+        String s = Integer.toString(n,3);
+        s = new StringBuilder(s).reverse().toString();
+        return Integer.parseInt(s,3);
     }
 }
