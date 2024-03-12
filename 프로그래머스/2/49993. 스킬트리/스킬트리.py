3b7298e774ea09c5e2e65b838f851fc435e42b66
JJONGHYUNI <whdgusdl8792@gmail.com>
2024-03-12 17:33:53 +0900

    [level 2] Title: 스킬트리, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.py
@@ -0,0 +1,16 @@
+def solution(skill, skill_trees):
+    answer = len(skill_trees)
+    sk = skill.strip()
+    for skill_tree in skill_trees:
+        temp = []
+        
+        for cur in skill_tree.strip():
+            if cur in sk:
+                temp.append(cur)
+        for i in range(len(temp)):
+            if temp[i] != sk[i]:
+                print(temp)
+                answer -= 1
+                break
+            
+    return answer
