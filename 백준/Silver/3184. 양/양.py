0397ab072318e773ced774ac62cab755005a3953
JJONGHYUNI <whdgusdl8792@gmail.com>
2024-04-18 17:59:00 +0900

    [Silver I] Title: 양, Time: 136 ms, Memory: 35112 KB -BaekjoonHub

--- 백준/Silver/3184. 양/양.py
+++ 백준/Silver/3184. 양/양.py
@@ -1,38 +1,39 @@
-# start 16:02
-# end   16:19
 import sys
 sys.setrecursionlimit(10 ** 6)
-
 input = sys.stdin.readline
 
 r, c = map(int, input().split())
-
+d = [-1, 1, 0, 0]
+wolf, sheep = 0, 0
 board = [list(input().strip()) for _ in range(r)]
-d = [0, 0, -1, 1]
+w, s = 0, 0
 visited = [[0] * c for _ in range(r)]
-sheep, wolf = 0, 0
+
 def dfs(y, x):
-    global sheep, wolf
+    global wolf, sheep
     visited[y][x] = 1
 
-    if board[y][x] == 'o':
-        sheep += 1
-    elif board[y][x] == 'v':
-        wolf += 1
-    
     for i in range(4):
         dy = y + d[i]
         dx = x + d[3 - i]
         if 0 <= dy < r and 0 <= dx < c and not visited[dy][dx] and board[dy][dx] != '#':
+            if board[dy][dx] == 'v':
+                wolf += 1
+            elif board[dy][dx] == 'o':
+                sheep += 1
             dfs(dy, dx)
-ansS, ansW = 0, 0
+
 for i in range(r):
     for j in range(c):
-        sheep, wolf = 0, 0
-        if not visited[i][j] and board[i][j] != '#':
+        if board[i][j] != '#' and not visited[i][j]:
+            sheep, wolf = 0, 0
+            if board[i][j] == 'v':
+                wolf += 1
+            elif board[i][j] == 'o':
+                sheep += 1
             dfs(i, j)
-        if sheep > wolf:
-            ansS += sheep
-            continue
-        ansW += wolf
-print(ansS, ansW)
+            if sheep > wolf:
+                s += sheep
+                continue
+            w += wolf
+print(s, w)
