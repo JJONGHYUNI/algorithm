5766e8a6b6a21235a281aeec45ac0d9de1990040
이종현 <whdgusdl8792@gmail.com>
2024-10-18 21:23:39 +0900

    [Bronze II] Title: 공 바꾸기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+arr = [i for i in range(1, n + 1)]
+for _ in range(m):
+  a, b = map(int, input().split())
+  tmp = arr[b - 1]
+  arr[b - 1] = arr[a - 1]
+  arr[a - 1] = tmp
+
+print(*arr)
